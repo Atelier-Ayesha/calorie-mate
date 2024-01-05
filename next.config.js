29db9c92fc0d480f7109964ecb1b6117bb43114f
SONGDAM <herb7761@gmail.com>
2024-01-05 23:50:30 +0900

    [init]: repo initalize

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const nextConfig = {
+	reactStrictMode: true,
+	compiler: {
+		emotion: true,
+	},
+	webpack(config) {
+		const fileLoaderRule = config.module.rules.find(rule =>
+			rule.test?.test?.('.svg')
+		);
+		config.module.rules.push(
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/, // *.svg?url
+			},
+			{
+				test: /\.svg$/i,
+				issuer: fileLoaderRule.issuer,
+				resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+				use: ['@svgr/webpack'],
+			}
+		);
+
+		fileLoaderRule.exclude = /\.svg$/i;
+
+		return config;
+	},
+};
+
+module.exports = nextConfig;
