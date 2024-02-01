7c5b28d10aac0283520b1191988790af728cadb4
SONGDAM <herb7761@gmail.com>
2024-02-01 21:29:54 +0900

    fix: vanila extract 이슈 해결

--- next.config.js
+++ next.config.js
@@ -5,28 +5,6 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {
 	reactStrictMode: true,
-	webpack(config) {
-		const fileLoaderRule = config.module.rules.find(rule =>
-			rule.test?.test?.('.svg')
-		);
-		config.module.rules.push(
-			{
-				...fileLoaderRule,
-				test: /\.svg$/i,
-				resourceQuery: /url/, // *.svg?url
-			},
-			{
-				test: /\.svg$/i,
-				issuer: fileLoaderRule.issuer,
-				resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-				use: ['@svgr/webpack'],
-			}
-		);
-
-		fileLoaderRule.exclude = /\.svg$/i;
-
-		return config;
-	},
 };
 
 module.exports = withVanillaExtract(nextConfig);
--- style/global.css.ts
+++ style/global.css.ts
@@ -4,10 +4,13 @@ globalStyle('*', {
 	margin: 0,
 	padding: 0,
 	boxSizing: 'border-box',
-	fontFamily:
-		'font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;',
+	fontFamily: 'Pretendard Variable',
 });
 
 globalStyle('ol,ul', {
 	listStyle: 'none',
+});
+
+globalStyle('html , body', {
+	height: '100%',
 });
