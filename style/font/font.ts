70de3fc2a4a120a11248d06f0b573d763d1413f2
drunken_neoguri <developneoguri@gmail.com>
2024-05-01 14:42:55 +0900

    [Chore] 폰트 유틸 파일 추가

---
+++ style/font/font.ts
@@ -0,0 +1,122 @@
+export const font = {
+	light10: {
+		fontWeight: 400,
+		fontSize: '10px',
+		lineHeight: '14px',
+	},
+	light12: {
+		fontWeight: 400,
+		fontSize: '12px',
+		lineHeight: '16px',
+	},
+	light14: {
+		fontWeight: 400,
+		fontSize: '14px',
+		lineHeight: '18px',
+	},
+	light16: {
+		fontWeight: 400,
+		fontSize: '16px',
+		lineHeight: '20px',
+	},
+	light18: {
+		fontWeight: 400,
+		fontSize: '18px',
+		lineHeight: '24px',
+	},
+	light20: {
+		fontWeight: 400,
+		fontSize: '20px',
+		lineHeight: '26px',
+	},
+	light22: {
+		fontWeight: 400,
+		fontSize: '22px',
+		lineHeight: '28px',
+	},
+	light24: {
+		fontWeight: 400,
+		fontSize: '24px',
+		lineHeight: '30px',
+	},
+	medium10: {
+		fontWeight: 500,
+		fontSize: '10px',
+		lineHeight: '14px',
+	},
+	medium12: {
+		fontWeight: 500,
+		fontSize: '12px',
+		lineHeight: '16px',
+	},
+	medium14: {
+		fontWeight: 500,
+		fontSize: '14px',
+		lineHeight: '18px',
+	},
+	medium16: {
+		fontWeight: 500,
+		fontSize: '16px',
+		lineHeight: '20px',
+	},
+	medium18: {
+		fontWeight: 500,
+		fontSize: '18px',
+		lineHeight: '24px',
+	},
+	medium20: {
+		fontWeight: 500,
+		fontSize: '20px',
+		lineHeight: '26px',
+	},
+	medium22: {
+		fontWeight: 500,
+		fontSize: '22px',
+		lineHeight: '28px',
+	},
+	medium24: {
+		fontWeight: 500,
+		fontSize: '24px',
+		lineHeight: '30px',
+	},
+	semibold10: {
+		fontWeight: 600,
+		fontSize: '10px',
+		lineHeight: '14px',
+	},
+	semibold12: {
+		fontWeight: 600,
+		fontSize: '12px',
+		lineHeight: '16px',
+	},
+	semibold14: {
+		fontWeight: 600,
+		fontSize: '14px',
+		lineHeight: '18px',
+	},
+	semibold16: {
+		fontWeight: 600,
+		fontSize: '16px',
+		lineHeight: '20px',
+	},
+	semibold18: {
+		fontWeight: 600,
+		fontSize: '18px',
+		lineHeight: '24px',
+	},
+	semibold20: {
+		fontWeight: 600,
+		fontSize: '20px',
+		lineHeight: '26px',
+	},
+	semibold22: {
+		fontWeight: 600,
+		fontSize: '22px',
+		lineHeight: '28px',
+	},
+	semibold24: {
+		fontWeight: 600,
+		fontSize: '24px',
+		lineHeight: '30px',
+	},
+};
