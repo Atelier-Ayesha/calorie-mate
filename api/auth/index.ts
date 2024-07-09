9d3166e4ab8821dc8ae8d431887b781a10e87ef0
drunken_neoguri <developneoguri@gmail.com>
2024-07-09 20:12:48 +0900

    [Chore] Auth API 로직에 내용 로그인, 본인 인증 등의 로직 추가 #9

--- api/auth/index.ts
+++ api/auth/index.ts
@@ -1,14 +1,49 @@
 import { AuthAPI } from '@/types/auth';
 import instance from '../intercepter';
-import { use } from 'react';
 
-export const register = (credential: AuthAPI.ISignUp) => {
+export const register = (credential: AuthAPI.ICredential) => {
 	const { confirmPassword, ...rest } = credential;
 
 	return instance({
 		url: '/user/auth/register',
 		data: {
 			...rest,
+		},
+	});
+};
+
+export const login = (credential: AuthAPI.TLogin) => {
+	return instance({
+		url: '/user/auth/login',
+		data: {
+			credential,
+		},
+	});
+};
+
+export const verify = (credential: AuthAPI.TUserMail) => {
+	return instance({
+		url: '/user/auth/verify',
+		data: {
+			credential,
+		},
+	});
+};
+
+export const sendmail = (credential: AuthAPI.TUserMail) => {
+	return instance({
+		url: '/user/auth/sendmail',
+		data: {
+			credential,
+		},
+	});
+};
+
+export const resetpassword = (credential: AuthAPI.TResetPassword) => {
+	return instance({
+		url: '/user/auth/resetpassword',
+		data: {
+			credential,
 		},
 	});
 };
